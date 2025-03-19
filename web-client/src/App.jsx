9daff33ed2ef66301b02fe303b853c4bd848087f
Compuute <160307775+Compuute@users.noreply.github.com>
2025-03-19 20:01:51 +0100

    Lagt till contract-läsning/skrivning och person-hantering med Ethereum

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -1,109 +1,46 @@
 import { ethers } from "ethers";
 import "./App.css";
-import { useState, useCallback } from "react";
-import { useEffect } from "react";
-import { abi, address } from "./config";
-import { Persons } from "./components/Persons";
-import { AddPerson } from "./components/AddPerson";
+import { address, abi } from "./config";
+import { useState, useEffect } from "react";
 
 if (window.ethereum) {
   window.provider = new ethers.BrowserProvider(window.ethereum);
 } else {
-  console.error("No web3 provider detected, please install...");
+  console.error("No web3 provider detected, please install MetaMask!");
 }
 
 function App() {
   const [readContract, setReadContract] = useState();
   const [writeContract, setWriteContract] = useState();
-  const [wallet, setWallet] = useState({
-    account: undefined,
-    balance: "",
-  });
-  const [persons, setPersons] = useState([]);
-
-  const updateWallet = async (accounts) => {
-    const balance = formatBalance(
-      await window.ethereum.request({
-        method: "eth_getBalance",
-        params: [accounts[0], "latest"],
-      }),
-    );
-
-    setWallet({ account: accounts[0], balance: balance });
-  };
-
-  const formatBalance = (rawBalance) => {
-    const balance = (parseInt(rawBalance) / 1000000000000000000).toFixed(5);
-    return balance;
-  };
-
-  const populatePersons = useCallback(async () => {
-    let indexes = [];
-    try {
-      indexes = await readContract.getIndexList();
-    } catch (error) {
-      console.error(error);
-    }
-
-    const tempArray = [];
-    for (let i = 0; i < indexes.length; i++) {
-      const person = await readContract.persons(indexes[i]);
-      if (person && person.id > 0) {
-        tempArray.push(person);
-      }
-    }
-
-    setPersons(tempArray);
-  }, [readContract]);
 
   useEffect(() => {
+    console.log(readContract, writeContract);
     if (readContract && writeContract) return;
 
-    const getProviders = async () => {
+    const getProvider = async () => {
       const provider = new ethers.BrowserProvider(window.ethereum);
-
-      // Create read contract - For reading stuff for free
+      // Create a read contract instance - For reading  stuff for free
       const rContract = new ethers.Contract(address, abi, provider);
       setReadContract(rContract);
 
+      // Create a write contract instance to be signed by metamask, not free
       const signer = await provider.getSigner();
-      // Create write contract to be signed by metamask, not free
       const wContract = new ethers.Contract(address, abi, signer);
       setWriteContract(wContract);
 
       let accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
-
-      updateWallet(accounts);
+      updateWallet(accounts[0]);
     };
 
-    getProviders();
+    getProvider();
   }, []);
 
-  useEffect(() => {
-    if (wallet.account && readContract) {
-      populatePersons();
-    }
-  }, [readContract, wallet.account, populatePersons]);
+  const connectWallet = async () => {
+    const accounts = await window.ethereum.request({});
+  };
 
-  return (
-    <>
-      <p>{wallet.account}</p>
-      <p>{wallet.balance}</p>
-
-      <AddPerson
-        writeContract={writeContract}
-        getAllPersons={populatePersons}
-      />
-
-      <Persons
-        persons={persons}
-        writeContract={writeContract}
-        populatePerson={populatePersons}
-      />
-    </>
-  );
+  return <>Hej !</>;
 }
-
 export default App;
